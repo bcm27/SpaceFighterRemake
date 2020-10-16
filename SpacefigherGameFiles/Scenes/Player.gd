00840bdb16cf4f9c5fdfc5f5b56ed567887f5497
= <bcm27@live.com>
2020-10-15 22:53:03 -0500

    renamed world scene to testlevel

--- SpacefigherGameFiles/Scenes/Player.gd
+++ SpacefigherGameFiles/Scenes/Player.gd
@@ -9,7 +9,9 @@ var healUse = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(true)
 	set_physics_process(true)
+	
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
